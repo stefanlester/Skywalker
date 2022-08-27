3439849cd13a7962eac4d8c65831ce08582f6241
Stefan <stefanamoah001@gmail.com>
2022-08-27 20:38:55 +0200

    refactor: simplify tests using table tests

--- render/render_test.go
+++ render/render_test.go
@@ -6,47 +6,48 @@ import (
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "no error rendering non-existent go template, when one is expected"},
+	{"go_page", "jet", "home", false, "error rendering jet template"},
+	{"go_page_no_template", "jet", "no-file", true, "no error rendering non-existent jet template, when one is expected"},
+	{"invalid_render_engine", "foo", "home", true, "no error rendering with invalid rendering engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	w := httptest.NewRecorder()
+	for _, e := range pageData {
+		r, err := http.NewRequest("GET", "/some-url", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	testRenderer.Renderer = "go"
-	testRenderer.RootPath = "./testdata"
+		w := httptest.NewRecorder()
 
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+		testRenderer.Renderer = e.renderer
+		testRenderer.RootPath = "./testdata"
 
-	err = testRenderer.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existenet template",err)
-	}
-
-	testRenderer.Renderer = "jet"
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = testRenderer.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Error rendering non-existenet jet template",err)
-	}
-
-	testRenderer.Renderer = ""
-	err = testRenderer.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("No error returned while rendering with invalid renderer specified",err)
+		err = testRenderer.Page(w, r, e.template, nil, nil)
+		if e.errorExpected {
+			if err == nil {
+				t.Errorf("%s: %s: expected error, but none was returned", e.name, e.errorMessage)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("%s: %s: %s: expected error, but none was returned", e.name, e.errorMessage, err.Error())
+			}
+		}
 	}
 }
 
 func TestRender_GoPage(t *testing.T) {
-	w:= httptest.NewRecorder()
+	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "/some-url", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -62,7 +63,7 @@ func TestRender_GoPage(t *testing.T) {
 }
 
 func TestRender_JetPage(t *testing.T) {
-	w:= httptest.NewRecorder()
+	w := httptest.NewRecorder()
 	r, err := http.NewRequest("GET", "/url", nil)
 	if err != nil {
 		t.Fatal(err)
@@ -75,4 +76,3 @@ func TestRender_JetPage(t *testing.T) {
 		t.Fatal(err)
 	}
 }
-
