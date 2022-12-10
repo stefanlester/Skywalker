e4a2a76ae3a8f66684138698f6928104ae4b6b55
STEFAN AMOAH <stefanamoah001@gmail.com>
2022-12-11 00:17:23 +0100

    add random string function

--- helpers.go
+++ helpers.go
@@ -1,7 +1,28 @@
 package skywalker
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
 
+const (
+	randomString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
+
+// gnerates a random string length n from values in the const randomString
+func (c *Skywalker) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
+
+// CreateDirIf NotExsit creates a new directory if it doesn't exist.
 func (c *Skywalker) CreateDirIfNotExist(path string) error {
 	const mode = 0755
 
@@ -15,6 +36,7 @@ func (c *Skywalker) CreateDirIfNotExist(path string) error {
 	return nil
 }
 
+// CreateFileIfNotExist creates a new file at path if it does not exist.
 func (c *Skywalker) CreateFileIfNotExist(path string) error {
 	var _, err = os.Stat(path)
 
