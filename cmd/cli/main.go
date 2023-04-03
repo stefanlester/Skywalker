a02cdfcad59eaa5874efb69a8bca2244fa1abd7c
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-04-03 20:43:43 +0200

    feat: migrations make func

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -26,6 +26,15 @@ func main() {
 	case "version":
 		color.Yellow("Application version: " + version)
 
+	case "make":
+		if arg2 == "" {
+			exitGracefully(errors.New("make requires a subcommand: (migration|model|handler)"))
+		}
+		err = doMake(arg2, arg3) // do make func resides in make.go
+ 		if err != nil {
+			exitGracefully(err)
+		}
+
 	default:
 		log.Println(arg2, arg3)
 	}
