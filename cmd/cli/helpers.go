5c1fa74b9f6af5b4d14fae74a7163c9bf5187336
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-04-04 18:03:32 +0200

    helper for make migration functionality

---
+++ cmd/cli/helpers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func setup() {
+	err := godotenv.Load()
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	path, err := os.Getwd()
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	sky.RootPath = path
+	sky.DB.DataType = os.Getenv("DATABASE_TYPE")
+
+}
