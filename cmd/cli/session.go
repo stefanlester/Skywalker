cefc66aac08687d88f608e041c6cb06db29c8cfd
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-05-09 23:16:07 +0200

    chore: do session table

---
+++ cmd/cli/session.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doSessionTable() error {
+	dbType := sky.DB.DataType
+
+	if dbType == "mariadb" {
+		dbType = "mysql"
+	}
+
+	if dbType == "postgresql" {
+		dbType = "postgres"
+	}
+
+	fileName := fmt.Sprintf("%d_create_sessions_table", time.Now().UnixMicro())
+
+	upFile := sky.RootPath + "/migrations/" + fileName + "." + dbType + ".up.sql"
+	downFile := sky.RootPath + "/migrations/" + fileName + "." + dbType + ".down.sql"
+
+	err := copyFilefromTemplate("templates/migrations/"+dbType+"_session.sql", upFile)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyDataToFile([]byte("drop table sessions"), downFile)
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = doMigrate("up", "")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	return nil
+}
