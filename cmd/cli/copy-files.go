9a76616b807084b71c97c8a5d023ca278eab1531
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-06-05 22:23:17 +0200

    chore: file exists checker

--- cmd/cli/copy-files.go
+++ cmd/cli/copy-files.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"embed"
+	"errors"
 	"io/ioutil"
+	"os"
 )
 
-// go:embed templates
+//go:embed templates
 var templateFS embed.FS
 
 func copyFilefromTemplate(templatePath, targetFile string) error {
-	// TODO: check to ensure file does not already exist
+	if fileExists(targetFile) {
+		return errors.New(targetFile + " already exists!")
+	}
 
 	data, err := templateFS.ReadFile(templatePath)
 	if err != nil {
@@ -30,4 +34,11 @@ func copyDataToFile(data []byte, to string) error {
 		return err
 	}
 	return nil
+}
+
+func fileExists(fileToCheck string) bool {
+	if _, err := os.Stat(fileToCheck); os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
