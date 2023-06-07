af3d2e36b92a73896875c1e902932ad5dab69e78
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-06-07 23:55:06 +0200

    chore: write json func from arbitrary data

--- response-utils.go
+++ response-utils.go
@@ -24,3 +24,25 @@ func (s *Skywalker) ReadJSON(w http.ResponseWriter, r *http.Request, data interf
 
 	return nil
 }
+
+// WriteJSON writes json from arbitrary data
+func (s *Skywalker) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
