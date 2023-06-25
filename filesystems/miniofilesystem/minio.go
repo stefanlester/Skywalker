6fc374292afd3576646eca5ca0ab4869334cc496
STEFAN AMOAH <stefanamoah001@gmail.com>
2023-06-25 20:27:15 +0200

    code org

--- filesystems/miniofilesystem/minio.go
+++ filesystems/miniofilesystem/minio.go
@@ -12,6 +12,8 @@ import (
 	"github.com/stefanlester/skywalker/filesystems"
 )
 
+// Minio is the overall type for the minio filesystem, and contains
+// the connection credentials, endpoint, and the bucket to use
 type Minio struct {
 	Endpoint string
 	Key      string
@@ -21,6 +23,8 @@ type Minio struct {
 	Bucket   string
 }
 
+// getCredentials generates a minio client using the credentials stored in
+// the Minio type
 func (m *Minio) getCredentials() *minio.Client {
 	client, err := minio.New(m.Endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(m.Key, m.Secret, ""),
@@ -33,6 +37,7 @@ func (m *Minio) getCredentials() *minio.Client {
 	return client
 }
 
+// Put transfers a file to the remote file system
 func (m *Minio) Put(fileName, folder string) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -50,7 +55,8 @@ func (m *Minio) Put(fileName, folder string) error {
 	return nil
 }
 
-// List all file systems
+// List returns a listing of all files in the remote bucket with the
+// given prefix, except for files with a leading . in the name
 func (m *Minio) List(prefix string) ([]filesystems.Listing, error) {
 	var listing []filesystems.Listing
 
